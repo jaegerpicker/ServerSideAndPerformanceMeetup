35cdafb0805c72e179d6bdccb08b6f78ac2c2e0c
Shawn Campbell <scampbell@vetsfirstchoice.com>
2015-05-18 12:10:43 -0400

    Started swift version

---
+++ CashRegister-May2015/ShawnCampbellSwift/CashRegister/CashRegister/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  CashRegister
+//
+//  Created by Shawn Campbell on 5/18/15.
+//  Copyright (c) 2015 Team Awesome McPants. All rights reserved.
+//
+
+import Foundation
+
+
+println("Hello, World!")
+
