6bd79ec140c56c07d00e3ace10d106b7d5fe6a4f
Shawn Campbell <scampbell@vetsfirstchoice.com>
2015-05-29 00:02:37 -0400

    added the template for the 2048 clojure solution

---
+++ 2048-June2015/ShawnCampbellClojure/codeeval_2048/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to codeeval_2048
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ 2048-June2015/ShawnCampbellClojure/codeeval_2048/test/codeeval_2048/core_test.clj
@@ -0,0 +1,7 @@
+(ns codeeval-2048.core-test
+  (:require [clojure.test :refer :all]
+            [codeeval-2048.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
