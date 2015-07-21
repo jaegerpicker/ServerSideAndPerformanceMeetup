b284613a5f3a7ddd806be9708b12d941fe81b974
Shawn Campbell <scampbell@vetsfirstchoice.com>
2015-07-21 19:43:33 -0400

    Updated clojure example

--- 2048-June2015/ShawnCampbellClojure/codeeval_2048/src/codeeval_2048/core.clj
+++ 2048-June2015/ShawnCampbellClojure/codeeval_2048/src/codeeval_2048/core.clj
@@ -7,22 +7,27 @@
   ; Read each line ignoring empty ones
   (doseq [line (remove empty? (line-seq rdr))]
     (parseLine line)))
-
+; First in the function cascade
+; Parses a line of the game board
 (defn parseLine
   [line]
   (let direction (get_direction line))
   (let grid_size (get_grid_size line))
   (println (do_move direction grid_size line 0)))
 
+; Get the direction of the game move
 (defn get_direction
   [line]
   (nth (split line #";") 1)
   )
 
+; Get the size of the grid
 (defn get_grid_size
   [line]
   (nth (split line #";") 2))
 
+
+; Starts the move
 (defn do_move
   [direction grid_size line movement]
   (let grid_lines (nth (split line #";") 3))
@@ -30,11 +35,13 @@
   (for [i (range grid_size)] (get_line grid direction (movement * i)))
   )
 
+; Grab the line to process
 (defn get_line
   [grid_lines direction line_no]
   (if (= direction "RIGHT")
     (ret_line (nth grid_lines line_no)))
   (if (= direction "LEFT")
+    ; Gah this is ugly and if I didn't suck at clojure I'd know a better way
     (reverse (ret_line (reverse (nth grid_lines line_no)))))
   (if (= direction "DOWN")
     (ret_line (get_column grid_lines line_no)))
@@ -42,6 +49,8 @@
      (reverse (ret_line (reverse (get_column grid_lines line_no)))))
   )
 
+
+; Grab indiviual columns to work on
 (defn get_column
   [grid_lines line_no]
   (map #(nth % line_no) grid_lines)
@@ -55,14 +64,23 @@
   (for [i (range (count non_zero_vals))] (combine_indexes non_zero_vals i (+ i 1)))
   )
 
+; Basic Utilities functions below here
+
+
+
+; Filter out certain the passed in index passed in
+; used to skip zero's
 (defn index-exclude
   [r ex]
   (filter #(not (ex %)) (range r)))
 
+; dissociate indexes of nonzero values
 (defn dissoc-idx
   [v & ds]
   (map v (index-exclude (count v) (into #{} ds))))
 
+; So this crazy thing is poorly named but basically it returns a 0, the static value
+; or the Combined values, basically aggegrating all the info together
 (defn combine_indexes
   [line current_index next_index]
 
