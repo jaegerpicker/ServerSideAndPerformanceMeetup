ececf410caaf2d1d548d50dabb98068e7755cfdd
Shawn Campbell <scampbell@vetsfirstchoice.com>
2015-10-27 12:00:22 -0400

    fsharp cleanup

---
+++ CryptoPals-Sept2015/FSharp-Shawn-Cryptopals/FSharp-Cryptopals-Shawn/FSharp-Cryptopals-Shawn/Main.fs
@@ -0,0 +1,12 @@
+﻿namespace FSharp_Cryptopals_Shawn
+open System
+open AppKit
+
+module main =
+    [<EntryPoint>]
+    let main args =
+        NSApplication.Init ()
+        let app = NSApplication.SharedApplication
+        app.Delegate <- new AppDelegate ()
+        app.Run ()
+        0
