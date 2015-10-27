acc58938bcddb64b4d09b36320721fbff0bd0cb1
Shawn Campbell <jaegerpicker@gmail.com>
2015-10-26 21:58:49 -0400

    Finished CSharp Example

---
+++ CryptoPals-Sept2015/Cryptopals-Shawn-CSharp/Cryptopals-Shawn-CSharp/Util.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cryptopals_Shawn_CSharp
+{
+    public static class Util
+    {
+        public static byte[] HexToBytes(string hex)
+        {
+            var bytes = new List<byte>();
+            for (int i = 0; i < hex.Length; i += 2)
+            {
+                var v = byte.Parse(hex.Substring(i, 2), System.Globalization.NumberStyles.HexNumber);
+                bytes.Add(v);
+            }
+            return bytes.ToArray();
+        }
+
+        public static string ToHex(byte[] bytes)
+        {
+            var sb = new StringBuilder(bytes.Length * 2);
+            foreach(var b in bytes)
+            {
+                sb.Append(b.ToString("x2"));
+            }
+            return sb.ToString();
+        }
+    }
+}
